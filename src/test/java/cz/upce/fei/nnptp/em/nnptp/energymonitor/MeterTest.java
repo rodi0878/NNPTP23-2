bb9fc3c08d09c4a316f0cb432542dc86535c461e
petrj99 <100368883+petrj99@users.noreply.github.com>
2023-11-09 20:14:26 +0100

    Commet removal

--- src/test/java/cz/upce/fei/nnptp/em/nnptp/energymonitor/MeterTest.java
+++ src/test/java/cz/upce/fei/nnptp/em/nnptp/energymonitor/MeterTest.java
@@ -1,7 +1,3 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package cz.upce.fei.nnptp.em.nnptp.energymonitor;
 
 import java.util.ArrayList;
@@ -12,10 +8,6 @@ import java.time.LocalDateTime;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-/**
- *
- * @author Petr Jiruše
- */
 public class MeterTest {
 
     public MeterTest() {
