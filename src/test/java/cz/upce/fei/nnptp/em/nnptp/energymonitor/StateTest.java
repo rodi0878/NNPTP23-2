33386d751d55def23fc38f3d7a9571c0bf5fa6c3
Dominik Fűrst <dominikfurst1@gmail.com>
2023-11-09 15:22:43 +0100

    Remove unused import

--- src/test/java/cz/upce/fei/nnptp/em/nnptp/energymonitor/StateTest.java
+++ src/test/java/cz/upce/fei/nnptp/em/nnptp/energymonitor/StateTest.java
@@ -5,7 +5,6 @@ import org.junit.jupiter.api.Test;
 
 import java.time.LocalDateTime;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
