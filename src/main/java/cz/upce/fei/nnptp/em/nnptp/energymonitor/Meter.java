32302aec31c0599b2a6f1295ee17b410c782a876
Kopec9 <kopecmar69@gmail.com>
2023-11-12 16:07:31 +0100

    Added control of changed meter

--- src/main/java/cz/upce/fei/nnptp/em/nnptp/energymonitor/Meter.java
+++ src/main/java/cz/upce/fei/nnptp/em/nnptp/energymonitor/Meter.java
@@ -1,5 +1,6 @@
 package cz.upce.fei.nnptp.em.nnptp.energymonitor;
 
+import cz.upce.fei.nnptp.em.nnptp.energymonitor.entity.ObservedTagsAndFlags;
 import cz.upce.fei.nnptp.em.nnptp.energymonitor.entity.ObservedValue;
 import java.time.LocalDateTime;
 import java.util.List;
@@ -69,22 +70,33 @@ public class Meter {
        //Calculate consumed power from selected measurements
         double totalConsumedElectricits = 0.0;
         
-        if (obVals == null || obVals.isEmpty()) {
+        if (observedValues == null || observedValues.isEmpty()) {
             return totalConsumedElectricits;
         }
         
         if (meterType == MeterType.CumulativeValue) {
             double previousValue = 0.0;
-            for (ObservedValue observedValue : obVals) {
+            for (ObservedValue observedValue : observedValues) {
                 if(observedValue.getLocalDateTime().compareTo(from) >= 0 && observedValue.getLocalDateTime().compareTo(to) <= 0){
                     if(previousValue != 0.0){
-                        totalConsumedElectricits += observedValue.getValue() - previousValue;
-                    }                   
-                    previousValue = observedValue.getValue();
+                        if(!observedValue.getTagsAndFlags().isEmpty()){
+                            totalConsumedElectricits += observedValue.getValue() - previousValue;
+                            for (ObservedTagsAndFlags tag : observedValue.getTagsAndFlags()) {
+                                if (tag instanceof ObservedTagsAndFlags.MeterReplacedJustAfterMeasurementTag newMeterTag) {
+                                    previousValue = newMeterTag.getMeterStartValue();
+                                }
+                            }
+                        }else{
+                            totalConsumedElectricits += observedValue.getValue() - previousValue;
+                            previousValue = observedValue.getValue();
+                        }                     
+                    }else{
+                        previousValue = observedValue.getValue();
+                    }                                     
                 }
             }
         }else if (meterType == MeterType.ActualValue) {
-            for (ObservedValue observedValue : obVals) {
+            for (ObservedValue observedValue : observedValues) {
                 if(observedValue.getLocalDateTime().compareTo(from) >= 0 && observedValue.getLocalDateTime().compareTo(to) <= 0){
                     totalConsumedElectricits += observedValue.getValue();
                 }
