d8f8129730d30e88f2c3596895297ef222aed133
petrj99 <100368883+petrj99@users.noreply.github.com>
2023-11-12 19:03:48 +0100

    Updated tests for recreated calculatePrice method

--- src/test/java/cz/upce/fei/nnptp/em/nnptp/energymonitor/MeterTest.java
+++ src/test/java/cz/upce/fei/nnptp/em/nnptp/energymonitor/MeterTest.java
@@ -1,15 +1,13 @@
 package cz.upce.fei.nnptp.em.nnptp.energymonitor;
 
+import cz.upce.fei.nnptp.em.nnptp.energymonitor.entity.ObservedTagsAndFlags;
 import cz.upce.fei.nnptp.em.nnptp.energymonitor.entity.ObservedValue;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 import org.junit.jupiter.api.Test;
-import org.mockito.Mockito;
 
 import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.Mockito.spy;
-import static org.mockito.Mockito.when;
 
 public class MeterTest {
 
@@ -17,36 +15,55 @@ public class MeterTest {
     }
 
     @Test
-    public void testCalculatePriceWithZeroConsumption() {
-        Energy energy = new Energy(Energy.EnergyType.HotWater);
-        energy.setPricePerMeasuredUnit(2.0);
-        
-        Meter meter = Mockito.mock(Meter.class);
-        when(meter.calculateConsumedElectricits()).thenReturn(0.0);
+    public void testCalculatePriceWithCumulativeMeter() {
+        Energy energy = new Energy(Energy.EnergyType.Electricity);
+        energy.setPricePerMeasuredUnit(10.0);
 
-        assertEquals(0.0, meter.calculatePrice());
+        List<ObservedValue> observedValues = new ArrayList<>();
+        observedValues.add(new ObservedValue(LocalDateTime.now(), 100.0));
+        observedValues.add(new ObservedValue(LocalDateTime.now().plusHours(1), 120.0));
+        observedValues.add(new ObservedValue(LocalDateTime.now().plusHours(2), 180.0));
+
+        Meter meter = new Meter(energy, null, Meter.MeterType.CumulativeValue, observedValues);
+        
+        double expectedPrice = 800.0;
+        assertEquals(expectedPrice, meter.calculatePrice());
     }
 
     @Test
-    public void testCalculatePriceWithSpecificConsumption() {
-        Energy energy = new Energy(Energy.EnergyType.Electricity);
-        energy.setPricePerMeasuredUnit(0.15);
-        
-        LocalDateTime time1 = LocalDateTime.now();
-        LocalDateTime time2 = time1.plusHours(3);
-        LocalDateTime time3 = time1.plusHours(7);
+    public void testCalculatePriceWithActualMeter() {
+        Energy energy = new Energy(Energy.EnergyType.Gas);
+        energy.setPricePerMeasuredUnit(20.0);
 
         List<ObservedValue> observedValues = new ArrayList<>();
-        observedValues.add(new ObservedValue(time1, 100.0));
-        observedValues.add(new ObservedValue(time2, 120.0));
-        observedValues.add(new ObservedValue(time3, 180.0));
+        observedValues.add(new ObservedValue(LocalDateTime.now(), 50.0));
+        observedValues.add(new ObservedValue(LocalDateTime.now().plusHours(1), 80.0));
+        observedValues.add(new ObservedValue(LocalDateTime.now().plusHours(2), 100.0));
 
-        Meter realMeter = new Meter(energy, null, Meter.MeterType.CumulativeValue, observedValues);
-        Meter meterSpy = Mockito.spy(realMeter);
+        Meter meter = new Meter(energy, null, Meter.MeterType.ActualValue, observedValues);
+
+        double expectedPrice = 4600.0;
+        assertEquals(expectedPrice, meter.calculatePrice());
+    }
+
+    @Test
+    public void testCalculatePriceWithPriceChange() {
+        Energy energy = new Energy(Energy.EnergyType.HotWater);
+        energy.setPricePerMeasuredUnit(2.0);
         
-        when(meterSpy.calculateConsumedElectricits()).thenReturn(80.0);
+        List<ObservedValue> observedValues = new ArrayList<>();
+        ObservedValue value1 = new ObservedValue(LocalDateTime.now(), 200);
+        ObservedValue value2 = new ObservedValue(LocalDateTime.now().plusHours(1), 350);
+
+        ObservedTagsAndFlags priceChange = new ObservedTagsAndFlags.UnitPriceChangedJustAfterMeasurementTag(5.0);
+        value2.getTagsAndFlags().add(priceChange);
+
+        observedValues.add(value1);
+        observedValues.add(value2);
+
+        Meter meter = new Meter(energy, null, Meter.MeterType.ActualValue, observedValues);
         
-        double expectedPrice = (180 - 100) * 0.15;
-        assertEquals(expectedPrice, meterSpy.calculatePrice());
+        double expectedPrice = 2150.0;
+        assertEquals(expectedPrice, meter.calculatePrice());
     }
 }
