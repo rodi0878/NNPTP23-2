2dd4c413ff8838cf120219a5f954ba4f79ba7576
Jan Stehno <honzastehno@email.cz>
2023-12-03 12:36:21 +0100

    Persistence saveState method implementation

--- src/main/java/cz/upce/fei/nnptp/em/nnptp/energymonitor/persistence/Persistence.java
+++ src/main/java/cz/upce/fei/nnptp/em/nnptp/energymonitor/persistence/Persistence.java
@@ -1,15 +1,37 @@
 package cz.upce.fei.nnptp.em.nnptp.energymonitor.persistence;
 
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 import cz.upce.fei.nnptp.em.nnptp.energymonitor.State;
-import java.io.File;
+
+import java.io.*;
 
 /**
- *
+ * Persistence - save and load state of all
+ * ConnectionPoints, Meters and Distributions
  */
 public class Persistence {
+    private final Gson gson = new GsonBuilder().setPrettyPrinting().create();
+
     public void saveState(State state, File file) {
-        // TODO
+        if (fileExists(file)) {
+            try (Writer writer = new FileWriter(file)) {
+                gson.toJson(state, writer);
+                System.out.println("State saved successfully.");
+            } catch (IOException e) {
+                System.err.println("Failed to save state: " + e.getMessage());
+            }
+        } else {
+            System.err.println("Failed to save state.");
+        }
     }
-    
-    // TODO loadState
+
+    public void loadState(File file) {
+        //TODO
+    }
+
+    private boolean fileExists(File file) {
+        //TODO
+        return true;
+    }
 }
